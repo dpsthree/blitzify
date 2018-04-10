285ea4647dd265a8b6d74531e18824d1bacef8f8
Paul Spears <dpsthree@gmail.com>
2018-04-10 14:37:32 -0500

    handle bad repo path
    refactor tag handling code to named function

--- src/blitzify.ts
+++ src/blitzify.ts
@@ -4,10 +4,13 @@ import { Config } from './config';
 
 export function createBlitzList(args: Config): Promise<void> {
   return git.Repository.open(args.pathToRepo)
-    .then(repo => {
-      return git.Tag.list(repo)
-        .then(tagList => {
-          console.log('tagList', tagList);
-        });
+    .then(processTags)
+    .catch(err => console.error(`Unabled to open repository at: ${args.pathToRepo}`, err));
+}
+
+function processTags(repo: git.Repository) {
+  return git.Tag.list(repo)
+    .then(tagList => {
+      console.log('tagList', tagList);
     });
 }
