43d8a3536e2f6b20aa28f5e5c91ff8f42a6521f0
Paul Spears <dpsthree@gmail.com>
2018-04-10 14:30:32 -0500

    test hello world step

--- src/blitzify.ts
+++ src/blitzify.ts
@@ -1,5 +1,12 @@
+import * as git from 'nodegit';
 import { Config } from './config';
 
-export function createBlitzList(args: Config): Promise<void> {
-  
+export function createBlitzList(_args: Config): Promise<void> {
+  return git.Repository.open('.')
+    .then(repo => {
+      return git.Tag.list(repo)
+        .then(tagList => {
+          console.log('tagList', tagList);
+        });
+    });
 }
